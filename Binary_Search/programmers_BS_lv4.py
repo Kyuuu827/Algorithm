b6e5b276ff786cbc465b4b3e3c1472db4377cc5b
Kyuuu827 <lck0827@gmail.com>
2022-01-23 03:39:27 +0900

    MODIFY: 프로그래머스_이진탐색_Lv4 풀이

--- Binary_Search/programmers_BS_lv4.py
+++ Binary_Search/programmers_BS_lv4.py
@@ -28,5 +28,27 @@ def solution(distance, rocks, n):
     return answer
 
 # 두 번째 풀이
+def solution(distance, rocks, n):
+    position = [0] + sorted(rocks) + [distance] # [0, 2, 11, 14, 17, 21, 25]
+    start = 1
+    end = distance
+
+    while start < end:
+        mid = (start+end) // 2
+        cnt = 0
+        i = 0
+        j = 1
+        while j <= len(position)-1 :
+            if position[j]-position[i] < mid:
+                cnt += 1
+                j += 1
+            else:
+                i = j
+                j += 1
+        if cnt > n:
+            end = mid
+        else:
+            start = mid + 1
+    return start - 1
 
 print(solution(25, [2, 14, 11, 21, 17], 2))
