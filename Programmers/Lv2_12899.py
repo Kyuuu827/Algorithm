f1dab378fa73babed638bd09248dfd9550f61cb0
Kyuuu827 <lck0827@gmail.com>
2022-02-20 00:45:24 +0900

    124 나라의 숫자 문제 풀이

---
+++ Programmers/Lv2_12899.py
@@ -0,0 +1,20 @@
+# 124 나라의 숫자
+# 첫 번째 풀이
+def solution(n):
+    if n <= 3:
+        return '124'[n-1]
+    else:
+        q, r = divmod(n-1, 3)
+        return solution(q) + '124'[r]
+
+# 두 번째 풀이
+def solution2(n):
+    num = ['1', '2', '4']
+    answer = ''
+
+    while n > 0:
+        n -= 1
+        answer = num[n % 3] + answer
+        n //= 3
+
+    return answer
